fe33caa8270ed21f8e83d17b63302d0c3659aff7
Christian D. Lamothe <c.lamothe25@gmail.com>
2017-07-26 00:06:47 +0000

    walkthrough

--- script.js
+++ script.js
@@ -11,17 +11,38 @@ $(document).ready(function() {
           //console.log(response.sources[2]);
           var sources = response.sources;
           //console.log(sources[2]);
-          var html = "<select class='form-control' id='source'>";
+          var html = "<select class='form-control' id='sourceOption'>";
           $.each(sources, function(index, source) {
               //console.log(source);
-              html += "<option value='1'>" + source.name + "</option>";
+              html += "<option value='" + source.id + "'>" + source.name + "</option>";
           });
           html += "</select>";
+         // console.log(html);
           $(".form-group").html(html);
        }
    });
    
-   $('select').val()
+   $("#source").submit(function(event) {
+       event.preventDefault();//tell \s to do this instead
+       var id = $('#sourceOption').val();
+       var url = "https://newsapi.org/v1/articles";
+       var data = {apiKey:"a2951ada0edd40f1a1b000a054ace8c5", source: id};
+       $.ajax({
+           url: url,
+           data: data,
+           type: "GET",
+           success: function(response) {
+              // console.log(response);
+               var articles = response.articles;
+               var html = "<ul class='list-group'>";
+               $.each(articles, function(index, article) {
+                  html += "<li class='list-item'>" + article.title + "</li>"; 
+               });
+               html += "</ul>";
+               $("#articles").html(html);
+           }
+       });
+   });
 });
 
 //
